c5d9bfa16367073d58f060100e93261d50e23c1b
Htet Aung Paing <aaronmoriee602@gmail.com>
2020-12-04 16:11:10 +0630

    new

--- js/script.js
+++ js/script.js
@@ -1,43 +1,30 @@
 $(document).ready(function(){
-	if($(".description").length){
-		ScrollReveal().reveal('.description', { delay: 500 });
-	}
-	if($("#st__num").length){
-		counter();	
-	}
-	if($("#heart-toggle").length){
-        $("#heart-toggle i").each(function(v,k){
-            k.addEventListener("click",function(){
-                if(hasClass(k,"far fa-heart")){
-                    k.classList.replace("far","fas");
-                }else{
-                    k.classList.replace("fas","far");
-                }
-            });
-        });
- 
-	}
+	$wind=$(window);
+	$header=$("#header__nav");
+	$toggle=$(".toggle-button");
+	
+	toggle_btn($header,$wind,$toggle);
+	$wind.resize(function(){
+		toggle_btn($header,$wind,$toggle);
+	});
+	$toggle.click(function(){
+		$header.toggleClass("toggle__btn-click");
+	});
 });
-function counter() {
-    let stnum = +$("#st__num").data("number");
-    let tenum = +$("#te__num").data("number");
-    let awnum = +$("#aw__num").data("number");
-    let speed = 20;
-    setInterval(function() {
-        let stnum_html = +$("#st__num").text();
-        if (stnum_html < stnum) {
-            $("#st__num").text(Math.floor(stnum_html + (stnum / speed)));
-        }
-        let tenum_html = +$("#te__num").text();
-        if (tenum_html < tenum) {
-            $("#te__num").text(Math.floor(tenum_html + (tenum / speed)));
-        }
-        let awnum_html = +$("#aw__num").text();
-        if (awnum_html < awnum) {
-            $("#aw__num").text(Math.ceil(awnum_html + (awnum / speed)));
-        }
-    }, 100);
+
+function toggle_btn($header,$wind,$toggle){
+	if($wind.width()<768){
+		$header.css({left:"-500px"})
+		$toggle.css({display:"block"});
+	}else{
+		$header.css({left:"0px"})
+		$toggle.css({display:"none"});
+	}
 }
-function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-}
+
+let typed=new Typed("#typed",{
+	strings:["Web Developer","Freelancer"],
+	loop:true,
+	typeSpeed:50,
+	backSpeed:50
+});
